26b8b0e75ec698de3bb37727512ec19c8aa6cb20
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:07:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "huddle-house-connec-45850",
+  url: "https://huddle-house-connec-45850.botics.co",
+  api: "https://huddle-house-connec-45850.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
